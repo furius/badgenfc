c78ccdb51ade2c072302d64b5547c8b338b3e210
furius <orlandelli.m@gmail.com>
2013-03-13 14:37:03 +0100

    new version with multi page

--- helloworld.js
+++ helloworld.js
@@ -1,32 +1,3 @@
-function getTimbrature(gname) {
-	console.debug("enter getTimbrature")
-	var Timbratura = StackMob.Model.extend({ schemaName: 'timbraturaonline' });
-	var Timbrature = StackMob.Collection.extend({ model: Timbratura });
-	var myTimbrature = new Timbrature();
-	var q = new StackMob.Collection.Query();
-	q.equals('group_name', gname);
-	q.setRange(0,39).orderDesc('createddate');
-	console.debug("after q.equals");
-	var esito="not evaluated";
-	myTimbrature.query(q, {
-		success: function(model) {
-			
-			var myJSON = model.toJSON();
-			console.debug(myJSON);
-			console.debug(myJSON.length);
-			
-			esito="ok";
-			writeTimbrature(myJSON);
-			//document.getElementById("hello").innerHTML = mygJSON.action;
-			//writeTable();
-	},
-		error: function(model, response) {
-			console.debug(response);
-			esito="error";
-			document.getElementById("hello").innerHTML = esito
-	}});
-
-}
 
 function checkGroup(gname,gpwd) {
 	var Group = StackMob.Model.extend({ schemaName: 'group' });
@@ -40,7 +11,9 @@ function checkGroup(gname,gpwd) {
 			var myJSONGroup = model.toJSON();
 			console.debug(myJSONGroup);
 			if (myJSONGroup[0].password==gpwd) {
-				getTimbrature(gname);
+				//getTimbrature(gname);
+				group_name=gname;
+				login_OK();
 			} else {
 				document.getElementById("error").innerHTML = "Wrong name or password!";
 			}
@@ -58,9 +31,26 @@ function checkGroup(gname,gpwd) {
 function start() {
 	//document.getElementById('click').onclick = getTimbrature;
 	document.getElementById('login').onclick = validate;
+	document.getElementById("previous").style.visibility = 'hidden';
+	document.getElementById("next").style.visibility = 'hidden';
 	
 }
 window.onload = start;
+var group_name;
+var current_page = 0;
+var total_records;
+var rec_per_page = 20;
+
+function login_OK() {
+	document.getElementById("login_frame").innerHTML = '<div id="login_frame"></div>';
+	total_records = GetRecordsNumber();
+	current_page = 1;
+	getTimbrature(false);
+	renderButtons();
+}
+
+
+
 
 function sleep(ms)
 {
@@ -69,11 +59,105 @@ function sleep(ms)
 	while (new Date().getTime() < dt.getTime());
 }
 
+function validate(){
+	var gname = document.getElementById('gname').value;
+	var gpwd = document.getElementById('gpwd').value;
+	checkGroup(gname, gpwd);
+	
+}
+
+
+function getTimbrature(reset) {
+	start_i = rec_per_page*(current_page-1) + 1;
+	end_i = rec_per_page*current_page - 1;
+	var gname = group_name;
+	console.debug("enter getTimbrature")
+	var Timbratura = StackMob.Model.extend({ schemaName: 'timbraturaonline' });
+	var Timbrature = StackMob.Collection.extend({ model: Timbratura });
+	var myTimbrature = new Timbrature();
+	var q = new StackMob.Collection.Query();
+	q.equals('group_name', gname);
+	q.setRange(start_i,end_i).orderDesc('createddate');
+	console.debug("after q.equals");
+	var esito="not evaluated";
+	myTimbrature.query(q, {
+		success: function(model) {
+			
+			var myJSON = model.toJSON();
+			console.debug(myJSON);
+			console.debug(myJSON.length);
+			if (reset) {document.getElementById("table").innerHTML = '<div id="content"></div>'};
+			esito="ok";
+			content_replacement = writeTimbrature(myJSON);
+			document.getElementById("content").innerHTML = content_replacement;
+
+			//document.getElementById("hello").innerHTML = mygJSON.action;
+			//writeTable();
+	},
+		error: function(model, response) {
+			console.debug(response);
+			esito="error";
+			document.getElementById("hello").innerHTML = esito
+	}});
+
+}
+
+function GetRecordsNumber() {
+	//Define your Todo class once on the page.
+	var Timbratura = StackMob.Model.extend({ schemaName: 'timbraturaonline' });
+	//Now, create a collection object and attach it to your previously created model (i.e. Todo)
+	var Timbrature = StackMob.Collection.extend({ model: Timbratura }); 
+	//create an instance of your Item collection
+	var myTimbrature = new Timbrature();
+	//set up the query you want to get the count for
+	var q = new StackMob.Collection.Query();
+	q.equals('group_name', group_name);
+	//call count
+	myTimbrature.count(q, {
+	  success: function(count) {
+		//console.log('count is:' + count );
+		total_records = count * 1;
+		renderButtons();
+		//document.getElementById("hello").innerHTML = "total records: " + record_number ;
+		//addCode(gname,2)
+	  }
+	});
+}
+
+function renderButtons() {
+	document.getElementById("previous").style.visibility = 'hidden';
+	document.getElementById("next").style.visibility = 'hidden';
+
+	if (current_page == 1 && total_records > rec_per_page ) {
+		//document.getElementById("previous").style.visibility = 'hidden';
+		document.getElementById("next").style.visibility = 'visible';
+	}
+	if (current_page > 1 ) {
+		document.getElementById("previous").style.visibility = 'visible';
+		if (total_records > rec_per_page*current_page) {
+			document.getElementById("next").style.visibility = 'visible';
+		}
+	}	
+	
+	//}
+}
+
+function getNextPage() {
+	current_page ++ ;
+	renderButtons();
+	getTimbrature(true);
+}
+
+function getPreviousPage() {
+	current_page -- ;
+	renderButtons();
+	getTimbrature(true);
+}
+
+
 function writeTimbrature(timbList) {
-    var newPage = "<html><head><title>Results</title>";
-	newPage += '<link rel="stylesheet" href="mystyle.css">'
-    newPage += "</head><body>";
-	newPage += '<div class="myframe">';
+    var newPage = "" ;
+	newPage += '<div class="myframe" id="table">';
 	newPage +='<img src="res/badgenfc_icon.png" style="float:left; margin:5px;">';
     newPage += "<h3>Check-in check-out table</h3>";
 	
@@ -110,18 +194,9 @@ function writeTimbrature(timbList) {
 		}
 	   newPage += '</table>';
 	   newPage += '</div>';
-    newPage += "</body></html>";
-    // write it in one blast
-    document.write(newPage);
-    // close writing stream
-    document.close( );
-}
+		return newPage ;
+	}
 
 
-function validate(){
-	var gname = document.getElementById('gname').value;
-	var gpwd = document.getElementById('gpwd').value;
-	checkGroup(gname, gpwd);
-	
-}
+
 
